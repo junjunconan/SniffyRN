dea850897a3cd456808d3ba6d4440058cb1f4b65
Owen Xi <junjunconan@gmail.com>
2018-02-23 16:47:23 +1100

    Add required packages

--- android/app/src/main/java/com/sniffyrn/MainApplication.java
+++ android/app/src/main/java/com/sniffyrn/MainApplication.java
@@ -3,6 +3,10 @@ package com.sniffyrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNSharePackage(),
+            new MapsPackage(),
+            new LinearGradientPackage(),
+            new FastImageViewPackage()
       );
     }
 
--- src/containers/Nearby.js
+++ src/containers/Nearby.js
@@ -9,6 +9,9 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import * as UserActions from '../actions/user';
+
+import MapView from 'react-native-maps';
+
 import Config from '../Config';
 
 class Nearby extends Component {
@@ -29,6 +32,7 @@ class Nearby extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <MapView style={styles.map} />
       </View>
     );
   }
@@ -49,5 +53,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
